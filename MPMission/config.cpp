2fb027e4141fe933b536364216c996d3f9adc8b0
suff0cati0n <nlovett0218@gmail.com>
2022-04-25 15:17:50 -0400

    Added a couple RHS mines to the config

--- MPMission/config.cpp
+++ MPMission/config.cpp
@@ -15,9 +15,12 @@ class CfgMine
 		"rhs_mine_m2a3b_trip_mag",
 		"rhs_mine_glasmine43_bz_mag",
 		"rhs_mine_glasmine43_hz_mag",
-		"rhs_mine_ozm72_b_mag",
+		"rhs_ec75_sand_mag",
 		"rhs_ec200_sand_mag",
 		"rhs_ec400_sand_mag",
+		"rhs_ec75_mag",
+		"rhs_ec200_mag",
+		"rhs_ec400_mag",
 		"APERSBoundingMine_Range_Mag",
 		"APERSMine_Range_Mag",
 		"APERSTripMine_Wire_Mag",
@@ -33,8 +36,11 @@ class CfgMine
 		"rhsusf_m112_mag",
 		"rhs_charge_M2tet_x2_mag",
 		"rhs_mine_ozm72_a_mag",
+		"rhs_mine_ozm72_b_mag",
+		"rhs_mine_pmn2_mag",
 
 		//Lower weights
+		//rhs_mine_sm320_green_mag
 		"rhs_mine_sm320_green_mag",
 		"rhs_mine_sm320_red_mag",
 		"rhs_mine_sm320_white_mag",
@@ -65,14 +71,17 @@ class CfgMine
 		class rhs_mine_m2a3b_trip_mag: DefaultMine 					{ CfgVehicleName = "rhs_mine_m2a3b_trip"; isRemote = false; };
 		class rhs_mine_glasmine43_bz_mag: DefaultMine  				{ CfgVehicleName = "rhs_mine_glasmine43_bz"; isRemote = false; };
 		class rhs_mine_glasmine43_hz_mag: DefaultMine 				{ CfgVehicleName = "rhs_mine_glasmine43_hz"; isRemote = false; };
-		class rhs_mine_ozm72_b_mag: DefaultMine  					{ CfgVehicleName = "rhs_mine_ozm72_b"; isRemote = false; };
-		class rhs_ec200_sand_mag: DefaultMine  						{ CfgVehicleName = "rhs_magazine_rhs_ec200_mag"; isRemote = true; };
-		class rhs_ec400_sand_mag: DefaultMine  						{ CfgVehicleName = "rhs_magazine_rhs_ec400_mag"; isRemote = true; };
+		class rhs_ec200_sand_mag: DefaultMine  						{ CfgVehicleName = "rhs_magazine_rhs_ec200_sand_mag"; isRemote = true; };
+		class rhs_ec400_sand_mag: DefaultMine  						{ CfgVehicleName = "rhs_magazine_rhs_ec400_sand_mag"; isRemote = true; };
+		class rhs_ec75_sand_mag: DefaultMine  						{ CfgVehicleName = "rhs_magazine_rhs_ec75_sand_mag"; isRemote = true; };
+		class rhs_ec75_mag: DefaultMine  							{ CfgVehicleName = "rhs_magazine_rhs_ec75_mag"; isRemote = true; };
+		class rhs_ec200_mag: DefaultMine  							{ CfgVehicleName = "rhs_magazine_rhs_ec200_mag"; isRemote = true; };
+		class rhs_ec400_mag: DefaultMine  							{ CfgVehicleName = "rhs_magazine_rhs_ec400_mag"; isRemote = true; };
 		class APERSBoundingMine_Range_Mag: DefaultMine  			{ CfgVehicleName = "APERSBoundingMine"; isRemote = false; };
 		class APERSMine_Range_Mag: DefaultMine  					{ CfgVehicleName = "APERSMine"; isRemote = false; };
 		class APERSTripMine_Wire_Mag: DefaultMine  					{ CfgVehicleName = "APERSTripMine"; isRemote = false; };
 		class SLAMDirectionalMine_Wire_Mag: DefaultMine  			{ CfgVehicleName = "SLAMDirectionalMine"; isRemote = false; };
-		class SatchelCharge_Remote_Mag: DefaultMine  				{ CfgVehicleName = "SatchelCharge_F"; isRemote = true; };
+		class SatchelCharge_Remote_Mag: DefaultMine  				{ CfgVehicleName = "SatchelCharge_F"; isRemote = true; weight = 50;};
 		class DemoCharge_Remote_Mag: DefaultMine  					{ CfgVehicleName = "DemoCharge_F"; isRemote = true; };
 		class IEDLandSmall_Remote_Mag: DefaultMine  				{ CfgVehicleName = "IEDLandSmall_F"; isRemote = true; };
 		class IEDLandBig_Remote_Mag: DefaultMine 					{ CfgVehicleName = "IEDLandBig_F"; isRemote = true; };
@@ -87,6 +96,8 @@ class CfgMine
 		class rhs_mine_msk40p_blue_mag: DefaultMine  				{ CfgVehicleName = "rhs_mine_msk40p_blue"; isRemote = false; weight = 10; };
 		class rhs_mine_msk40p_green_mag: DefaultMine  				{ CfgVehicleName = "rhs_mine_msk40p_green"; isRemote = false; weight = 10; };
 		class rhs_mine_ozm72_a_mag: DefaultMine  					{ CfgVehicleName = "rhs_mine_ozm72_a"; isRemote = false; };
+		class rhs_mine_ozm72_b_mag: DefaultMine  					{ CfgVehicleName = "rhs_mine_ozm72_b"; isRemote = false; };
+		class rhs_mine_pmn2_mag: DefaultMine  						{ CfgVehicleName = "rhs_mine_pmn2"; isRemote = false; };
 		class rhs_charge_M2tet_x2_mag: DefaultMine  				{ CfgVehicleName = "rhs_magazine_rhs_charge_tnt_x2_mag"; isRemote = true; };
 
 	};
@@ -101,7 +112,7 @@ class CfgMine
 	disarmActivationDistance = 4; //Distance in meters, play around with it to get the right setting. If you set it too close, the mine might go off.
 	//The prompt is activated whether the player is looking at the mine or not (would need further development to fix this)
 
-	territoryOnly = false; //Should mines only be allowed to be placed inside territories?
+	territoryOnly = false; //Should mines only be allowed to be placed inside territories? (not implemented)
 
 	//Sound ID for disarming
 	disarmingSound = "mine_dismantling";
